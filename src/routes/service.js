f4ff946682ad28d3956dd256ed652cedcbb7af08
First Last <email>
2024-11-22 15:12:07 -0700

    tests

--- src/routes/service.js
+++ src/routes/service.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { authRouter, setAuthUser } = require('./routes/authRouter.js');
-const orderRouter = require('./routes/orderRouter.js');
-const franchiseRouter = require('./routes/franchiseRouter.js');
-const version = require('./version.json');
-const config = require('./config.js');
+const { authRouter, setAuthUser } = require('./authRouter.js');
+const orderRouter = require('./orderRouter.js');
+const franchiseRouter = require('./franchiseRouter.js');
+const version = require('../version.json');
+const config = require('../config.js');
 
 const app = express();
 app.use(express.json());

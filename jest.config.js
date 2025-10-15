fb0dbb0e68c19a60dfb92c433fb332c841b8cb63
Tristen Bitter <tristenkbitter@gmail.com>
2025-10-14 20:33:07 -0600

    fixed lint errors again

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
-export default {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["json-summary", "lcov", "text", "clover"],
-  coverageThreshold: {
-    global: {
-      lines: 80
-    }
-  },
-  testEnvironment: "node",
-  transform: {}
+export const collectCoverage = true;
+export const coverageDirectory = "coverage";
+export const coverageReporters = ["json-summary", "lcov", "text", "clover"];
+export const coverageThreshold = {
+  global: { lines: 80 },
 };
+export const testEnvironment = "node";
+export const transform = {};

d91bd7f7bab3a0c5d0cb6b58fac6902a3b62d82a
TristenBitter <123581497+TristenBitter@users.noreply.github.com>
2025-10-28 11:48:06 -0600

    Update database password in config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
      //host: '127.0.0.1',
      host: 'host.docker.internal',
      user: 'root',
-     password: 'Dolphin',
+     password: 'Dolphin329',
      database: 'jwtpizza',
      connectTimeout: 60000,
    },

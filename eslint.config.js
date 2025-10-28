15fd0d51f0c17a29fd9378afd02520dcd89a7c9d
TristenBitter <123581497+TristenBitter@users.noreply.github.com>
2025-10-28 11:35:29 -0600

    Update and rename eslint.config.mjs to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+];

62e5da7bf1c22acb4ac264903c49dc5f02cdb627
TristenBitter <123581497+TristenBitter@users.noreply.github.com>
2025-10-28 11:41:38 -0600

    Refactor eslint.config.js for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,3 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 import js from '@eslint/js';
 import globals from 'globals';
 

6db0f2dce4dbbba3dd231a8aad651fe586dc9d61
TristenBitter <123581497+TristenBitter@users.noreply.github.com>
2025-11-13 17:46:30 -0700

    Add logging configuration to config.js
    Added a new logging section to the config.

--- src/config.js
+++ src/config.js
@@ -16,9 +16,15 @@ export const factory = {
   apiKey: "4a253a1c77e14bd3b0e8dcfcc594d434",
 };
 
-// Add this new metrics section
 export const metrics = {
-  source: 'jwt-pizza-service-dev',  // -dev suffix for development environment
+  source: 'jwt-pizza-service-dev',
   url: 'https://otlp-gateway-prod-us-east-2.grafana.net/otlp/v1/metrics',
   apiKey: '1429590:glc_eyJvIjoiMTU4MjExNyIsIm4iOiJzdGFjay0xNDI5NTkwLWludGVncmF0aW9uLWp3dC1waXp6YS1tZXRyaWNzIiwiayI6Ik0zMHBySTJZOEE2V3VqUDFRQkswMFA3OSIsIm0iOnsiciI6InByb2QtdXMtZWFzdC0wIn19',
+};
+
+export const logging = {
+  source: 'jwt-pizza-service-prod',
+  userId: 0,
+  url: '',
+  apiKey: '',
 };
